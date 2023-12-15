8fe2034e7646c2ed62ea78dd3e7616a28b4e40a9
Purvi kargathara <purvi.devmonk@gmail.com>
2023-12-15 11:39:54 +0530

    sub-main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './components/left/left.component';
+import { RightComponent } from './components/right/right.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
