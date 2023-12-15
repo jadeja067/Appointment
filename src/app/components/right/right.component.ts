bd84ec8831fee8ec5a20a36798ac7cd2d47b439e
Harikrishnasinh Zala <harikrishnasinh16@gmail.com>
2023-12-15 13:00:02 +0530

    Updated Right

--- src/app/components/right/right.component.ts
+++ src/app/components/right/right.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./right.component.css']
 })
 export class RightComponent implements OnInit {
-  datesArray:number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+  datesArray:any[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
   constructor() { }
 
   ngOnInit(): void {
