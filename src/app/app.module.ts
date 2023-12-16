7368b062c77363b8be97cb7e838f7b6042f0852d
Purvi kargathara <purvi.devmonk@gmail.com>
2023-12-16 11:12:14 +0530

    dsfge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,7 @@ import { RightComponent } from './components/right/right.component';
 import { ContainerComponent } from './components/container/container.component';
 import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
 import { FullCalendarModule } from '@fullcalendar/angular';
-<<<<<<< HEAD
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatDatepickerModule,
-} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-
-=======
 import { NgForm, ReactiveFormsModule } from '@angular/forms';
->>>>>>> origin/branch-right-side
 
 @NgModule({
   declarations: [
@@ -30,14 +21,7 @@ import { NgForm, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     CalendarModule,
-<<<<<<< HEAD
-    FullCalendarModule,
-    BrowserAnimationsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-=======
     FullCalendarModule,ReactiveFormsModule
->>>>>>> origin/branch-right-side
   ],
   providers: [],
   bootstrap: [AppComponent]
